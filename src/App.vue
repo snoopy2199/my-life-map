8700236f139e19dea338a4fd06d2f2d74e20552b
snoopy2199 <snoopy2199@gmail.com>
2018-06-11 21:00:01 +0800

    Adjust layout

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,35 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app" class="app">
+    <div class="app__sidebar">
+      <router-link to="/">Home</router-link>
     </div>
-    <router-view/>
+    <div class="app__main">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+  margin: 0;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+
+.app {
+  display: -webkit-flex;
+  display: flex;
+  height: 100vh;
+}
+.app__sidebar {
+  -webkit-flex: none;
+  flex: none;
+  width: 280px;
+  overflow-y: scroll;
+  background-color: $color-primary;
+  color: $color-text-on-primary;
+}
+.app__main {
+  -webkit-flex: 1;
+  flex: 1;
+  overflow-y: scroll;
 }
 </style>
