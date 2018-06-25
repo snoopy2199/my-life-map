db394e7c1b74d2b79c9d13e13bc54fa56d233454
snoopy2199 <snoopy2199@gmail.com>
2018-06-26 00:52:58 +0800

    Add message field

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-    home
+    <div class="home__message">
+      <span>
+        Say something~
+      </span>
+    </div>
   </div>
 </template>
 
@@ -9,3 +13,39 @@ export default {
   name: 'home',
 };
 </script>
+
+<style lang="scss">
+.home {
+  padding: 30px 50px;
+}
+
+.home__message {
+  display: flex;
+  position: relative;
+  height: 70px;
+  background: $color-secondary-light;
+  border-radius: 20px;
+  box-shadow: 0 1px 1px 0 rgba(60, 64, 67, .08), 0 1px 3px 0 rgba(60, 64, 67, .16);
+  padding: 20px 30px;
+
+  span {
+    display: flex;
+    align-items: center;
+  }
+}
+
+.home__message:after {
+  content: '';
+  position: absolute;
+  left: 0;
+  top: 70%;
+  width: 0;
+  height: 0;
+  border: 20px solid transparent;
+  border-right-color: $color-secondary-light;
+  border-left: 0;
+  border-bottom: 0;
+  margin-top: -10px;
+  margin-left: -20px;
+}
+</style>
